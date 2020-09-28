f384d15686dbabbc0234cecbe1ebcf0ac6fcc154
hypersmc <mhypers@gmail.com>
2020-09-28 21:23:47 +0200

    Initial commit

---
+++ src/main/java/me/hypersmc/jumpwatch/main/Main.java
@@ -0,0 +1,15 @@
+package me.hypersmc.jumpwatch.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
